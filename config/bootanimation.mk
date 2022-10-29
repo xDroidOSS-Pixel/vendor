339e00fe0c82556443ea7b5a59069b27dcd4a9d8
Asriadi Rahim <asriadirahim03@gmail.com>
2022-10-30 03:05:16 +0800

    vendor: bring back mnmlist bootanimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,4 +1,8 @@
 # xd. Bootanimation
+ifeq ($(XDROID_BOOT),)
+$(warning "xdroidUI: Using xd. Dark Bootanimation legacy")
+    PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/xd_boot_legacy.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
 ifeq ($(XDROID_BOOT),480)
 $(warning "xdroidUI: Using xd. Dark Bootanimation 480p")
     PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/xd_boot_480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
@@ -6,12 +10,20 @@ endif
 ifeq ($(XDROID_BOOT),720)
 $(warning "xdroidUI: Using xd. Dark Bootanimation 720p")
     PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/xd_boot_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
 ifeq ($(XDROID_BOOT),1080)
-$(warning "xdpUI: Using PE Monet Bootanimation 1080p")
-    PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/pe_boot_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+$(warning "xdpUI: Using mnmlist Bootanimation 1080p")
+    PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/xd_boot_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
 ifeq ($(XDROID_BOOT),1440)
-$(warning "xdpUI: Using PE Monet Bootanimation 1440p")
+$(warning "xdpUI: Using mnmlist Bootanimation 1440p")
+    PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/xd_boot_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+ifeq ($(XDROID_BOOT),M1080)
+$(warning "xdroidUI: Using Monet Bootanimation 1080p")
+    PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/pe_boot_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
+ifeq ($(XDROID_BOOT),M1440)
+$(warning "xdroidUI: Using Monet Bootanimation 1440p")
     PRODUCT_COPY_FILES += vendor/xdroid/bootanimation/pe_boot_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
 endif
